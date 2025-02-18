903d78ee3b0d9633e127fad5b9c8d94f6daf3785
Sohan Sharma <sohangamedev@gmail.com>
2025-02-19 00:13:30 +0530

    integrated zoom ui

--- Assets/MandelBrot.cs
+++ Assets/MandelBrot.cs
@@ -1,4 +1,6 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem.LowLevel;
 
 public class Fractal : MonoBehaviour
 {
@@ -7,8 +9,8 @@ public class Fractal : MonoBehaviour
     [SerializeField] ComputeShader _shader;
     private ComputeBuffer _buffer;
 
-    private int MAX_ITERATIONS = 100;
-    [SerializeField] double _zoom;
+    public int MAX_ITERATIONS = 100;
+    [SerializeField] float _zoom;
     [SerializeField] float _speed;
 
     [SerializeField] UnityEngine.Vector2 _offset;
@@ -16,7 +18,7 @@ public class Fractal : MonoBehaviour
     [Header("Texture")]
     [SerializeField] int _width = 100;
     [SerializeField] int _height = 100;
-
+    [SerializeField] TextMeshProUGUI _text;
     private RenderTexture _texture;
     private int kernel;
 
@@ -30,7 +32,7 @@ public class Fractal : MonoBehaviour
         _texture.Create();
 
 
-        _shader.FindKernel("CSMain");
+        kernel = _shader.FindKernel("CSMain");
         _buffer = new ComputeBuffer(_width * _height, sizeof(int));
         _shader.SetInt("_Width", _width);
         _shader.SetInt("_Height", _height);
@@ -47,20 +49,27 @@ public class Fractal : MonoBehaviour
         _shader.SetVector("_Offset", _offset);
         _shader.SetInt("MAX_ITERATIONS", MAX_ITERATIONS);
 
-        _shader.Dispatch(0, Mathf.CeilToInt(_width / 24.0f), Mathf.CeilToInt(_height / 24.0f), 1);
+        _shader.Dispatch(0, Mathf.CeilToInt(_width /16.0f), Mathf.CeilToInt(_height / 16.0f), 1);
 
+                                                             
+        if (Input.GetKey(KeyCode.A)) { _offset.x -= _speed / _zoom * Time.deltaTime; }
+        if (Input.GetKey(KeyCode.D)) { _offset.x += _speed / _zoom * Time.deltaTime; }
+        if (Input.GetKey(KeyCode.W)) { _offset.y += _speed / _zoom * Time.deltaTime; }
+        if (Input.GetKey(KeyCode.S)) { _offset.y -= _speed / _zoom * Time.deltaTime; }
 
-        if (Input.GetKey(KeyCode.A)) { _offset.x -= _speed / (float)_zoom * Time.deltaTime; }
-        if (Input.GetKey(KeyCode.D)) { _offset.x += _speed / (float)_zoom * Time.deltaTime; }
-        if (Input.GetKey(KeyCode.W)) { _offset.y += _speed / (float)_zoom * Time.deltaTime; }
-        if (Input.GetKey(KeyCode.S)) { _offset.y -= _speed / (float)_zoom * Time.deltaTime; }
-            
-        if (Input.GetKey(KeyCode.E)) { _zoom += _speed * _zoom * Time.deltaTime; MAX_ITERATIONS += 2; }
-        if (Input.GetKey(KeyCode.Q) && _zoom > 0.5) { _zoom -= _speed * _zoom * Time.deltaTime; MAX_ITERATIONS -= 2; }
+        if ((Input.GetKey(KeyCode.E) || Input.GetMouseButton(0)) && _zoom < 200000) 
+                          { _zoom += _speed * _zoom * Time.deltaTime; if (MAX_ITERATIONS < 2000) MAX_ITERATIONS += 2; }
+        if ((Input.GetKey(KeyCode.Q) || Input.GetMouseButton(1)) && _zoom > 0.35) { _zoom -= _speed * _zoom * Time.deltaTime; MAX_ITERATIONS -= 2; }
 
-        
+        if (Input.GetKey(KeyCode.Space))
+        {
+            MAX_ITERATIONS = 100;
+            _zoom = 1;
+            _offset = new Vector2(-0.5f, 0);
+        }
+
+        _text.text = ((int)_zoom).ToString() + "x";  
     }
-
     private void OnDisable()
     {
         _buffer.Dispose();
